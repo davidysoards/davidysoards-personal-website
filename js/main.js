9d09f3ab5fe28feec0115bdce9b0471d72d49bbf
David Soards <ninjasoards@Davids-MacBook-Pro.local>
2018-03-14 18:10:40 -0400

    started adding projects

--- js/main.js
+++ js/main.js
@@ -9,18 +9,43 @@ lightbox.option({
 });
 
 // hero homepage big head bubble tween
-var $headBalloon = $('.head-balloon'),
+const $headBalloon = $('.head-balloon'),
       $bighead = $('#bighead'),
       $balloon = $('#balloon'),
       $hello = $('#hello'),
+      $myNameIs = ('.hero.home h1'),
       headTl = new TimelineLite();
 
 headTl
-  .fromTo($bighead, 1.8, {x: '300%', autoAlpha: 0}, {x: '0%', autoAlpha: 1, ease: Back.easeOut.config(1)})
-  .fromTo($balloon, 0.7, {x: '+=30', y: '+=15'}, {x: 0, y: 0, autoAlpha: 1, ease: Power1.easeOut}, '-=0.3')
-  .to($hello, 0.4, {autoAlpha: 1}, '-=0.4');
-
+  .fromTo($bighead, 1.3, {y: '200%'}, {y: '0%', autoAlpha: 1, ease: Back.easeOut.config(1)})
+  .fromTo($balloon, 0.7, {x: '+=40', y: '+=20'}, {x: 0, y: 0, autoAlpha: 1, ease: Power1.easeOut}, '-=0.3')
+  .to($hello, 0.4, {autoAlpha: 1}, '-=0.4')
+  ;
+  
 // desk tween on portfolio page
 const $desk = $('.desk-illustration');
 
 TweenLite.fromTo($desk, 1.8, {x: '300%', autoAlpha: 0}, {x: '0%', autoAlpha: 1, ease: Back.easeOut.config(0.7)});
+
+//=SCROLL TO IDs
+
+// handle links with @href started with '#' only
+$(document).on('click', 'a[href^="#"]', function(e) {
+  // target element id
+  var id = $(this).attr('href');
+
+  // target element
+  var $id = $(id);
+  if ($id.length === 0) {
+      return;
+  }
+
+  // prevent standard hash navigation (avoid blinking in IE)
+  e.preventDefault();
+
+  // top position relative to the document
+  var pos = $id.offset().top - 60;
+
+  // animated top scrolling
+  $('body, html').animate({scrollTop: pos});
+});
