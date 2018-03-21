9b9e4e30716f9f97c18b6473cfcbe775c75023e0
David Soards <ninjasoards@Davids-MacBook-Pro.local>
2018-03-21 01:20:52 -0400

    about page css animations

--- js/main.js
+++ js/main.js
@@ -17,20 +17,26 @@ const $headBalloon = $('.head-balloon'),
       $bighead = $('#bighead'),
       $balloon = $('#balloon'),
       $hello = $('#hello'),
-      $myNameIs = ('.hero.home h1'),
       headTl = new TimelineLite();
-
+      
 headTl
-  .fromTo($bighead, 1.3, {y: '150%'}, {y: '0%', autoAlpha: 1, ease: Back.easeOut.config(1)})
-  .fromTo($balloon, 0.7, {x: '+=40', y: '+=20'}, {x: 0, y: 0, autoAlpha: 1, ease: Power1.easeOut}, '-=0.3')
-  .to($hello, 0.4, {autoAlpha: 1}, '-=0.4')
-  ;
-  
+.fromTo($bighead, 1.3, {y: '150%'}, {y: '0%', autoAlpha: 1, ease: Back.easeOut.config(1)})
+.fromTo($balloon, 0.7, {x: '+=40', y: '+=20'}, {x: 0, y: 0, autoAlpha: 1, ease: Power1.easeOut}, '-=0.3')
+.to($hello, 0.4, {autoAlpha: 1}, '-=0.4')
+;
+   
+// Scroll arrow all pages
+const $arrow = $('.arrow')
+TweenLite.fromTo($arrow, 1, {y: '-=18', autoAlpha: 0}, {y: 0, autoAlpha: 1, ease: Power1.easeOut, delay: 1.3});
+
+
 // desk tween on portfolio page
-const $desk = $('.desk-illustration');
+const $desk = $('#desk-illustration');      
+TweenLite.fromTo($desk, 1.7, {x: '300%', autoAlpha: 0}, {x: '0%', autoAlpha: 1, ease: Back.easeOut.config(1)});
 
-TweenLite.fromTo($desk, 1.8, {x: '300%', autoAlpha: 0}, {x: '0%', autoAlpha: 1, ease: Back.easeOut.config(0.7)});
-
+// headshot on about page
+const $headshot = $('#headshot');
+TweenLite.fromTo($headshot, 1.7, {scale: 0, autoAlpha: 0}, {scale: 1, autoAlpha: 1, ease: Back.easeOut.config(1)});
 
 /********** 
 =SCROLL TO IDs
@@ -50,7 +56,7 @@ $(document).on('click', 'a[href^="#"]', function(e) {
   e.preventDefault();
 
   // top position relative to the document
-  var pos = $id.offset().top - 60;
+  var pos = $id.offset().top - 77;
 
   // animated top scrolling
   $('body, html').animate({scrollTop: pos});
